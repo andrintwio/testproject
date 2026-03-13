d8b4aac5f008f4d2f681f86904537f5c1b956066
andrintwio <andrin@twio.tech>
2026-03-13 11:46:40 +0100

    Deleted test module

--- tw_pdf_customizations/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-    
-    tw_DUNS_nr_importer = fields.Char(string="DUNS-Nr Importer")
-    
--- tw_pdf_customizations/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-    
-    tw_EORI_nr = fields.Char(string="EORI", size=15)
-    
